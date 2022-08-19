2d82bc5941569965219ea4548781100833588b5e
Rychard Mazarin <rychardnbmazarin89@gmail.com>
2022-08-19 18:07:40 -0300

    Update __init__.py

--- formulario/__init__.py
+++ formulario/__init__.py
@@ -9,10 +9,10 @@ secrets.token_hex(16)
 Gera uma 'secret_key'
 """
 app.config['SECRET_KEY'] = 'dba3334567fe8cfcf7c36678c5587b36'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Le110784#'
-app.config['MYSQL_DB'] = 'db_cadastro'
+app.config['MYSQL_HOST'] = 'your localhost'
+app.config['MYSQL_USER'] = 'your user'
+app.config['MYSQL_PASSWORD'] = 'your password'
+app.config['MYSQL_DB'] = 'your DB'
 
 mysql = MySQL(app)
 
